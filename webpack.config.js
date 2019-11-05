ca70a97d7634e4150088a08b93838c883e8212ae
jimcheung <hi.jinhu.zhang@gmail.com>
2019-11-05 15:43:23 +0800

    fix deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 'use strict';
+
+const copyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
 // Document：https://www.yuque.com/easy-team/easywebpack 和 https://www.yuque.com/easy-team/egg-vue
 module.exports = {
-  plugins: {
-    imagemini: false
-  }
+  plugins: [
+    {
+      imagemini: false
+    },
+    new copyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'app/public/images'),
+        to: 'images'
+      }
+    ])
+  ]
 };
